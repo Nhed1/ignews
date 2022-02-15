f7ba3af16f443720cdbdc45029a551a6e9b6c4da
Leonardo Garreto <leonardogarreto@gmail.com>
2022-02-15 15:45:27 -0300

    capitulo 3 parte 1

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  return (
+    <button type="button" className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  );
+}
