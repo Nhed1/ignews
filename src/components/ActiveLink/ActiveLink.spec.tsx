ff187f0b09121d7500a851b572cbd4c68f80784d
leonardogarreto <leonardogarreto@gmail.com>
2022-05-07 10:46:05 -0300

    signInButtonClass

--- src/components/ActiveLink/ActiveLink.spec.tsx
+++ src/components/ActiveLink/ActiveLink.spec.tsx
@@ -1,12 +1,34 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import ActiveLink from ".";
 
-test("active link renders correctly", () => {
-  const { debug } = render(
-    <ActiveLink activeClassName="active" href="/">
-      <a>Home</a>
-    </ActiveLink>
-  );
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
 
-  debug();
+describe("ActiveLink component", () => {
+  it("renders correctly", () => {
+    render(
+      <ActiveLink activeClassName="active" href="/">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("Home")).toBeInTheDocument();
+  });
+
+  it("adds active class if the link is currently active", () => {
+    render(
+      <ActiveLink activeClassName="active" href="/">
+        <a>Home</a>
+      </ActiveLink>
+    );
+
+    expect(screen.getByText("Home")).toHaveClass("active");
+  });
 });
---
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { useSession } from "next-auth/react";
+import { SignInButton } from ".";
+
+jest.mock("next-auth/react");
+
+describe("SignInButton component", () => {
+  it("renders correctly when user is not authenticated", () => {
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
+  });
+
+  it("renders correctly when user is authenticated", () => {
+    render(<SignInButton />);
+
+    expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
+  });
+});
